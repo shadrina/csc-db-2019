ad3cee3108c9b0771d19cf60665b7aca3241a034
Anastasiya Shadrina <a.shadrina5@mail.ru>
2019-11-29 17:01:17 +0700

    Minor

--- docs/scheme.sql
+++ docs/scheme.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Pizza(
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     weight INT NOT NULL CHECK(weight > 0),
     cost INT NOT NULL CHECK(cost >= 0)
 );
 
 CREATE TABLE Product(
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL
 );
 
@@ -16,14 +16,14 @@ CREATE TABLE Stock(
 );
 
 CREATE TABLE Receipt(
-    pizza_id INT REFERENCES Pizza,
-    product_id INT REFERENCES Product,
+    pizza_id INT NOT NULL REFERENCES Pizza,
+    product_id INT NOT NULL REFERENCES Product,
     product_amount INT NOT NULL CHECK(product_amount > 0),
     UNIQUE(pizza_id, product_id)
 );
 
 CREATE TABLE Client(
-    id INT PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     phone_number TEXT UNIQUE NOT NULL,
     discount_amount INT NOT NULL CHECK(discount_amount >= 0 AND discount_amount <= 100) DEFAULT 0
 );
@@ -31,7 +31,7 @@ CREATE TABLE Client(
 CREATE TYPE OrderStatus AS ENUM ('принят', 'в обработке', 'отгружен', 'выполнен');
 
 CREATE TABLE OrderInfo(
-    id INT PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     client_id INT REFERENCES Client,
     address TEXT NOT NULL,
     status OrderStatus NOT NULL,
@@ -40,21 +40,21 @@ CREATE TABLE OrderInfo(
 );
 
 CREATE TABLE OrderDetails(
-    order_id INT REFERENCES OrderInfo,
-    pizza_id INT REFERENCES Pizza,
+    order_id INT NOT NULL REFERENCES OrderInfo,
+    pizza_id INT NOT NULL REFERENCES Pizza,
     amount INT NOT NULL CHECK (amount >= 0),
     UNIQUE(order_id, pizza_id)
 );
 
 CREATE TABLE Supplier(
-    id INT PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     phone_number TEXT UNIQUE
 );
 
 CREATE TABLE SupplierArrangement(
-    supplier_id INT REFERENCES Supplier,
-    product_id INT REFERENCES Product,
+    supplier_id INT NOT NULL REFERENCES Supplier,
+    product_id INT NOT NULL REFERENCES Product,
     cost INT NOT NULL CHECK(cost >= 0),
     UNIQUE(supplier_id, product_id)
 );
